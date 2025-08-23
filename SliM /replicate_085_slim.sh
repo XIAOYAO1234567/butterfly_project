891d10713f248ca21e4c21b36f95df2c4bca9a67
XIAOYAO1234567 <ap24175@qmul.ac.uk>
2025-08-23 09:57:05 +0800

    Update replicate_085_slim.sh

--- SliM /replicate_085_slim.sh
+++ SliM /replicate_085_slim.sh
@@ -12,3 +12,8 @@ outdir="SLiM_update/outputs/NC_069085.1/replicate/rep${rep}"
 mkdir -p "$outdir"
 
 slim -d outdir='"'"$outdir"'"' simulate_NC_085.slim
+
+# Batch Submit 100 Repetitive Tasks
+for rep in $(seq 1 100); do
+    qsub simulate_rep.sh $rep  
+done
